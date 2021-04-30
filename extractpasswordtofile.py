fe7ecaac244437a3853a457af98e6be3facd1822
Jasoncarterjoseph <46191199+Jasoncarterjoseph@users.noreply.github.com>
2021-04-30 09:49:45 +0530

    added comments

--- extractpasswordtofile.py
+++ extractpasswordtofile.py
@@ -1,22 +1,31 @@
 import re
+#funtion to split the strings on spaces
 def convert(string):
     li=list(string.split("\n"))
     return li
+#function to extract words that satisfy the password validity
 def extractpassword(li):
     pwd=[]
     for password in li:
+        #password lenght
         if(len(password)<8):
             continue
+        #password has alphabets
         elif not re.search("[a-z]", password):
             continue
+        #password has captialized alphabets
         elif not re.search("[A-Z]", password):
             continue
+        #password has numerics
         elif not re.search("[0-9]", password):
             continue
+        #password has special characters
         elif not re.search("[_@$]", password):
             continue
+        #password doesnt have space
         elif re.search("\s", password):
             continue
+        #if satisfy call condtions then append to list
         else:
             pwd.append(password)
             continue
@@ -27,6 +36,7 @@ li=convert(para)
 print(convert(para))
 print(extractpassword(li))
 pwds=extractpassword(li)
+#to write on text file
 with open("passlist.txt",'a') as f:
     for item in pwds:
         f.write(item)
